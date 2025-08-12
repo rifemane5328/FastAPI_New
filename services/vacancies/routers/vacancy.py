1850126649309d40f2a2f40bc87fce132a6878ed
Hewlet <zdanevych2011@gmail.com>
2025-08-12 19:01:21 +0300

    feat: added checks for authorization on each endpoint and print messages.Also added successful messages for delete endpoints

--- services/vacancies/routers/vacancy.py
+++ services/vacancies/routers/vacancy.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, status, HTTPException, Query, Depends
 
 from dependecies.session import AsyncSessionDep
 from models import Vacancy
+from models import User
 from services.vacancies.schemas.vacancy import VacancyResponseSchema, VacancyListResponseSchema, VacancyCreateSchema, \
     VacancyUpdateSchema
 from services.vacancies.query_builder.vacancy import VacancyQueryBuilder
@@ -10,6 +11,7 @@ from common.errors import EmptyQueryResult
 from common.pagination import PaginationParams
 from services.vacancies.errors import VacancyNotFound, ImpossibleRange
 from services.vacancies.schemas.filters import VacancyFilter
+from services.users.modules.manager import current_active_user
 
 vacancies_router = APIRouter()
 
@@ -20,9 +22,11 @@ async def get_vacancies(session: AsyncSessionDep,
                         title_filter: VacancyFilter = Depends(),
                         s_from: float = Query(0.00, description="salary_from"),
                         s_to: float = Query(25000.00, description="salary_to"),
+                        user: User = Depends(current_active_user)
                         ) -> VacancyListResponseSchema:
     try:
         vacancies = await VacancyQueryBuilder.get_vacancies(session, pagination_params, title_filter, s_from, s_to)
+        print(f"User {user.email} has sent a request")
         return VacancyListResponseSchema(items=vacancies)
     except EmptyQueryResult:
         raise HTTPException(status_code=status.HTTP_204_NO_CONTENT)
@@ -33,42 +37,52 @@ async def get_vacancies(session: AsyncSessionDep,
 
 
 @vacancies_router.get('/vacancy_by_id/{id}')
-async def get_vacancy_by_id(session: AsyncSessionDep, vacancy_id: int) -> VacancyResponseSchema:
+async def get_vacancy_by_id(session: AsyncSessionDep, vacancy_id: int,
+                            user: User = Depends(current_active_user)) -> VacancyResponseSchema:
     try:
         new_vacancy = await VacancyQueryBuilder.get_vacancy_by_id(session, vacancy_id)
+        print(f"User {user.email} has sent a request")
         return new_vacancy
     except VacancyNotFound as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
 
 
 @vacancies_router.post('/vacancies')
-async def create_vacancy(session: AsyncSessionDep, data: VacancyCreateSchema) -> VacancyResponseSchema:
+async def create_vacancy(session: AsyncSessionDep, data: VacancyCreateSchema,
+                         user: User = Depends(current_active_user)) -> VacancyResponseSchema:
     new_vacancy = await VacancyQueryBuilder.create_vacancy(session, data)
+    print(f"User {user.email} has sent a request")
     return new_vacancy
 
 
-@vacancies_router.delete('/vacancy_by_id/{id}', status_code=status.HTTP_204_NO_CONTENT)
-async def delete_vacancy_by_id(session: AsyncSessionDep, vacancy_id: int) -> None:
+@vacancies_router.delete('/vacancy_by_id/{id}', status_code=status.HTTP_204_NO_CONTENT,
+                         response_description=f"User was successfully deleted")
+async def delete_vacancy_by_id(session: AsyncSessionDep, vacancy_id: int,
+                               user: User = Depends(current_active_user)) -> None:
     try:
         await VacancyQueryBuilder.delete_vacancy_by_id(session, vacancy_id)
+        print(f"User {user.email} has sent a request")
     except VacancyNotFound as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
 
 
 @vacancies_router.patch('/vacancy_by_id/{id}', response_model=Vacancy, status_code=status.HTTP_200_OK)
-async def update_vacancy(session: AsyncSessionDep, vacancy_id: int, data: VacancyUpdateSchema) -> VacancyResponseSchema:
+async def update_vacancy(session: AsyncSessionDep, vacancy_id: int, data: VacancyUpdateSchema,
+                         user: User = Depends(current_active_user)) -> VacancyResponseSchema:
     try:
         vacancy = await VacancyQueryBuilder.update_vacancy(session, vacancy_id, data)
+        print(f"User {user.email} has sent a request")
         return vacancy
     except VacancyNotFound as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
 
 
 @vacancies_router.put('/vacancy_by_id/{vacancy_id}', response_model=Vacancy, status_code=status.HTTP_200_OK)
-async def update_vacancy_fully(session: AsyncSessionDep,
-                               vacancy_id: int, data: VacancyCreateSchema) -> VacancyResponseSchema:
+async def update_vacancy_fully(session: AsyncSessionDep, vacancy_id: int, data: VacancyCreateSchema,
+                               user: User = Depends(current_active_user)) -> VacancyResponseSchema:
     try:
         vacancy = await VacancyQueryBuilder.update_vacancy_fully(session, vacancy_id, data)
+        print(f"User {user.email} has sent a request")
         return vacancy
     except VacancyNotFound as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
--- services/workers/routers/worker.py
+++ services/workers/routers/worker.py
@@ -6,64 +6,77 @@ from common.errors import EmptyQueryResult
 from common.pagination import PaginationParams
 from dependecies.session import AsyncSessionDep
 from models import Worker
+from models import User
 from services.workers.query_builder.worker import WorkerQueryBuilder
 from services.workers.schemas.filters import WorkerFilter
 from services.workers.schemas.worker import (WorkerListResponseSchema, WorkerResponseSchema, WorkerCreateSchema,
                                              WorkerUpdateSchema)
 from services.workers.errors import WorkerNotFound
+from services.users.modules.manager import current_active_user
 
 workers_router = APIRouter()
 
 
-@workers_router.get('/workers')
+@workers_router.get('/workers', response_model=WorkerListResponseSchema)
 async def get_workers(session: AsyncSessionDep,
                       pagination_params: Annotated[PaginationParams, Depends(PaginationParams)],
-                      name_filter: WorkerFilter = Depends()) -> WorkerListResponseSchema:
+                      name_filter: WorkerFilter = Depends(),
+                      user: User = Depends(current_active_user)) -> WorkerListResponseSchema:
     try:
         workers = await WorkerQueryBuilder.get_workers(session, pagination_params, name_filter)
+        print(f"User {user.email} has sent a request")
         return WorkerListResponseSchema(items=workers)
     except EmptyQueryResult:
         raise HTTPException(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@workers_router.get('/worker_by_id/{id}')
-async def get_worker_by_id(session: AsyncSessionDep, worker_id: int) -> WorkerResponseSchema:
+@workers_router.get('/worker_by_id/{id}', response_model=Worker)
+async def get_worker_by_id(session: AsyncSessionDep, worker_id: int,
+                           user: User = Depends(current_active_user)) -> WorkerResponseSchema:
     try:
         worker = await WorkerQueryBuilder.get_worker_by_id(session, worker_id)
+        print(f"User {user.email} has sent a request")
         return worker
     except WorkerNotFound as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
 
 
-@workers_router.post('/workers', status_code=status.HTTP_201_CREATED)
-async def create_worker(session: AsyncSessionDep, data: WorkerCreateSchema) -> WorkerResponseSchema:
+@workers_router.post('/workers', status_code=status.HTTP_201_CREATED, response_model=WorkerResponseSchema)
+async def create_worker(session: AsyncSessionDep, data: Worker,
+                        user: User = Depends(current_active_user)) -> WorkerResponseSchema:
     new_worker = await WorkerQueryBuilder.create_worker(session, data)
+    print(f"User {user.email} has created a new worker")
     return new_worker
 
 
-@workers_router.delete('/worker_by_id/{id}', status_code=status.HTTP_204_NO_CONTENT)
-async def delete_worker_by_id(session: AsyncSessionDep, worker_id: int) -> None:
+@workers_router.delete('/worker_by_id/{id}', status_code=status.HTTP_204_NO_CONTENT,
+                       response_description=f"User was successfully deleted")
+async def delete_worker_by_id(session: AsyncSessionDep, worker_id: int,
+                              user: User = Depends(current_active_user)) -> None:
     try:
         await WorkerQueryBuilder.delete_worker(session, worker_id)
+        print(f"User {user.email} has sent a request")
     except WorkerNotFound as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
 
 
 @workers_router.patch('/worker_by_id/{worker_id}', response_model=Worker, status_code=status.HTTP_200_OK)
-async def update_worker(session: AsyncSessionDep,
-                        worker_id: int, data: WorkerUpdateSchema) -> WorkerResponseSchema:
+async def update_worker(session: AsyncSessionDep, worker_id: int, data: WorkerUpdateSchema,
+                        user: User = Depends(current_active_user)) -> WorkerResponseSchema:
     try:
         worker = await WorkerQueryBuilder.update_worker(session, worker_id, data)
+        print(f"User {user.email} has sent a request")
         return worker
     except WorkerNotFound as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
 
 
 @workers_router.put('/worker_by_id/{worker_id}', response_model=Worker, status_code=status.HTTP_200_OK)
-async def update_worker_fully(session: AsyncSessionDep,
-                              worker_id: int, data: WorkerCreateSchema) -> WorkerResponseSchema:
+async def update_worker_fully(session: AsyncSessionDep, worker_id: int, data: WorkerCreateSchema,
+                              user: User = Depends(current_active_user)) -> WorkerResponseSchema:
     try:
         worker = await WorkerQueryBuilder.update_worker_fully(session, worker_id, data)
+        print(f"User {user.email} has sent a request")
         return worker
     except WorkerNotFound as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
